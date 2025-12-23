a7b78b333559210a2531b40b42aab01d4260fe87
arthurbuilds <vnzwadf0937@outlook.com>
2025-12-23 14:11:09 +0800

    Wire up InvisiVote deployment script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedInvisiVote = await deploy("InvisiVote", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`InvisiVote contract: `, deployedInvisiVote.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_invisivote"; // id required to prevent reexecution
+func.tags = ["InvisiVote"];
